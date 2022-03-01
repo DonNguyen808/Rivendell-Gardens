82df086f63bc52ce37ee97bf24945ef044130876
DonNguyen808 <DonNguyen808@gmail.com>
2022-03-01 15:07:23 -0500

    added js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// pseudo code
+
+document.querySelector('#plantButton').addEventListener('click', plantImage)
+
+function plantImage() {
+    document.querySelector('#showPlant').innerText = "Showing Plant"
+}
+
+// dont forget #
