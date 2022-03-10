326465538a0b79b02521eb7039c9d6d5656c206d
DonNguyen808 <DonNguyen808@gmail.com>
2022-03-09 20:35:07 -0500

    redo rivendell

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// pseudo code
-
-document.querySelector('#plantButton').addEventListener('click', plantImage)
-
-function plantImage() {
-    document.querySelector('#showPlant').innerText = "Showing Plant"
-}
-
-// dont forget #
